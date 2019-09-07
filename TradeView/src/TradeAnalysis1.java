3c47af66aebeb3b648834dcd37c6b8cd729d10c6
asasson <sasson60@seas.upenn.edu>
2019-09-07 14:11:56 -0400

    Adding Analysis1 and Reader1 to work with "statements" download

--- TradeView/src/TradeAnalysis1.java
+++ TradeView/src/TradeAnalysis1.java
@@ -1,45 +1,45 @@
 import java.util.ArrayList;
 
-public class TradeAnalysis {
-	TradeReader TR;
-	
-	
-	public TradeAnalysis() {
-		this.TR =  new TradeReader("JointJun18toAug19.csv");
-		
+public class TradeAnalysis1 {
+	TradeReader1 TR1;
+
+
+	public TradeAnalysis1() {
+		this.TR1 =  new TradeReader1("trades3.csv");
+
 	}
-	
-	
+
+
 	public void allTrades() {
-		
-		for (Trade trade : TR.blotter) {
+
+		for (Trade trade : TR1.blotter) {
 			System.out.print(trade.getDate().year + "/" + trade.getDate().month + "/" + trade.getDate().day + " ");
 			System.out.print(trade.getProceeds());
 			System.out.print(trade.getDirection());
 			System.out.println(trade.getTicker());		
 		}
-		
+
 	}// end of allTrades method
-	
+
 	/**
 	 * Method to create arraylist of all distinct tickers
 	 * @return
 	 */
 	public ArrayList<String> distinctTickers(){
 		ArrayList<String> allTickers = new ArrayList<String>();
-		for (Trade trade : TR.blotter) {
+		for (Trade trade : TR1.blotter) {
 			if( allTickers.contains(trade.getTicker())) {
-				
+
 			} else {
 				allTickers.add(trade.getTicker());
 			}
 		} // end of for loop
-		
+
 		//System.out.println(allTickers);
 		return allTickers;
-		
+
 	} //end of alltickers
-	
+
 	/**
 	 * method to calculate current position for any given ticker
 	 * @param ticker
@@ -47,49 +47,49 @@ public class TradeAnalysis {
 	 */
 	public int positionCalc(String ticker) {
 		int currentPosition =0;
-		for (Trade trade : TR.blotter) {
+		for (Trade trade : TR1.blotter) {
 			if(trade.getTicker().equals(ticker)) {
-				if(trade.getDirection().equals("Bought")) {
+				if(trade.getDirection().equals("Buy")) {
 					currentPosition = currentPosition + trade.getSize();
 				} else {
 					currentPosition = currentPosition - trade.getSize();
 				}
-				
+
 			}
-			
+
 		}
 		//System.out.println("Position in " + ticker + " is " + currentPosition + " shares");
 		return currentPosition;
 	}
-	
+
 	/**
 	 * method to calculate pnl in closed trades in one ticker
 	 * @param ticker
 	 * @return
 	 */
 	public double pnlClosed(String ticker) {
-		
+
 		double tickerPnl = 0.00;
 		if(positionCalc(ticker) ==0) {
-			for (Trade trade : TR.blotter) {
+			for (Trade trade : TR1.blotter) {
 				if(trade.getTicker().equals(ticker)) {
-					if(trade.getDirection().equals("Bought")) {
+					if(trade.getDirection().equals("Buy")) {
 						tickerPnl = tickerPnl - trade.getProceeds();
 					} else {
 						tickerPnl = tickerPnl + trade.getProceeds();
 					}
 				}
 		}
-		
-		
+
+
 		}
 		tickerPnl = Math.round(tickerPnl * 100);
 		tickerPnl = tickerPnl/100;
-		
+
 		//System.out.println("PNL in " + ticker + " is " + tickerPnl);
 		return tickerPnl;
 	}
-	
+
 	/**
 	 * Calculates pnl on all closed trades
 	 * @param allTickers
@@ -97,29 +97,26 @@ public class TradeAnalysis {
 	 */
 	public double pnlTotalClosed(ArrayList<String> allTickers) {
 		double totalClosedPnl =0;
-		System.out.println("TICKER \t PNL" );
-		System.out.println("====== \t =========" );
 		for (String ticker : allTickers) {
 			double pnl = pnlClosed(ticker);
 			totalClosedPnl += pnl;
 			if(pnl != 0) {
-				
-				System.out.printf("%s \t %.2f \n", ticker, pnl);
+				System.out.println("PNL in " + ticker + " is " + pnl);
 			}
-			
-			
+
+
 		}
-		
+
 		totalClosedPnl = Math.round(totalClosedPnl * 100);
 		totalClosedPnl = totalClosedPnl/100;
 		System.out.println();
-		System.out.printf("The total pnl in closed trades is : %.2f \n", totalClosedPnl);
-		System.out.printf("This is inclusive of comissions paid of : %.2f \n", comissionPaid());
+		System.out.println("The total ytd pnl in closed trades is : " + totalClosedPnl);
+		System.out.println("This is inclusive of comissions paid of :" + comissionPaid());
 		System.out.println();
 		return totalClosedPnl;
-		
+
 	}
-	
+
 	/**
 	 * Calculates pnl on all closed trades
 	 * @param allTickers
@@ -137,10 +134,10 @@ public class TradeAnalysis {
 				unsortedPnl.add(pnl);
 				//System.out.println("PNL in " + ticker + " is " + pnl);
 			}
-			
-			
+
+
 		}
-		
+
 		for(int x = 0; x < unsortedPnl.size(); x++) {
 			double largest = unsortedPnl.get(0);
 			int index = 0;
@@ -150,17 +147,17 @@ public class TradeAnalysis {
 					index = i;
 		      } 
 		}
-			
+
 			sortedPnl.add(unsortedPnl.get(index));
 			sortedTickers.add(allTickers.get(index)); // uses index from above to add to sorted list of strings
 			unsortedPnl.set(index, (double) -1000000); //  // changes current largest to -1 to exclude from further calculation
 		}			
-			
-			
+
+
 		for(int i = 0; i < sortedTickers.size(); i++) {
 			System.out.println("PNL in " + sortedTickers.get(i) + " is " + sortedPnl.get(i));
 		}
-		
+
 		totalClosedPnl = Math.round(totalClosedPnl * 100);
 		totalClosedPnl = totalClosedPnl/100;
 		System.out.println();
@@ -168,9 +165,9 @@ public class TradeAnalysis {
 		System.out.println("This is inclusive of comissions paid of :" + comissionPaid());
 		System.out.println();
 		return totalClosedPnl;
-		
+
 	}
-	
+
 	/*
 	public ArrayList<String> wordSorter(ArrayList<String> unsortedStrings, ArrayList<Double> unsortedNumbers){
 		ArrayList<Double> sortedNumbers = new ArrayList<Double>();
@@ -193,56 +190,56 @@ public class TradeAnalysis {
 		return sortedStrings;
 		
 	} // end of wordSorter method */
-	
-	
+
+
 	public void openPositions(ArrayList<String> tickers) {
 		System.out.println("The following are your current open positions: ");
 		for (String ticker : tickers) {
 			int position = positionCalc(ticker);
 			if(position != 0) {
-				System.out.printf("%s \t %d shares \n", ticker, position);
+				System.out.println(ticker + ": " + position + " shares");
 			}
-			
+
 		}
 	}
-	
+
 	public double comissionPaid() {
 		double compaid = 0.00;
-		for (Trade trade : TR.blotter) {
+		for (Trade trade : TR1.blotter) {
 			double comission = trade.getComission();
 			compaid += comission;
 		}
-		
+
 		compaid = Math.round(compaid * 100);
 		compaid = compaid/100;
 		return compaid;
 	}
-	
-	
-	
 
-	
+
+
+
+
 	public static void main(String[] args) {
-		/*
-		TradeAnalysis TA = new TradeAnalysis();
-		//TA.allTrades();
+
+		TradeAnalysis1 TA1 = new TradeAnalysis1();
+		TA1.allTrades();
 		//TA.distinctTickers();
 		//TA.positionCalc("ROKU");
 		//TA.pnlClosed("TVIX");
 		//TA.positionCalc("LULU");
-		
-		TA.pnlTotalClosed(TA.distinctTickers());
-		TA.openPositions(TA.distinctTickers());
-		
-		*/
-		
+
+		TA1.pnlTotalClosed(TA1.distinctTickers());
+		TA1.openPositions(TA1.distinctTickers());
+
+
+
 		//TDate testDate = new TDate("05/09/2001");
 		//System.out.println(testDate.day);
 		//System.out.println(testDate.month);
 		//System.out.println(testDate.year);
-		
-		
-		
+
+
+
 	}
-	
+
 }
---
+++ TradeView/src/TradeReader1.java
@@ -0,0 +1,65 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Scanner;
+//use this for etrade downloaded document in the "statements section" 
+public class TradeReader1 {
+
+	ArrayList<Trade> blotter;
+
+	public TradeReader1(String filename){
+		File file = new File(filename);
+		blotter = new ArrayList<Trade>();
+
+		try {
+			Scanner scanner = new Scanner(file);
+			scanner.nextLine();
+
+			TDate date;
+			String direction;
+			String ticker;
+			int size;
+			double price;
+			double comission;
+			double proceeds;
+
+
+			while (scanner.hasNextLine()) {
+				String tradeRow = scanner.nextLine();
+				String[] columnData = tradeRow.split(",");
+
+				if((columnData[2].substring(0, 1)).equals("9")) {
+				} else {
+
+
+				date = new TDate((columnData[0]));
+				direction = columnData[1];
+				ticker = columnData[2];
+				size = Integer.parseInt(columnData[5]);
+				price = Double.parseDouble(columnData[6]);
+				comission = Double.parseDouble(columnData[7]);
+				proceeds = Double.parseDouble((columnData[8]).substring(0, columnData[8].length()-1));
+
+				Trade trade = new Trade(date, direction, ticker, size, price, comission, proceeds);
+				blotter.add(trade);
+
+
+
+
+				}
+
+			}
+
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+
+
+
+	}
+
+
+}
